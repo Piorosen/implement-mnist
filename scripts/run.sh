400e793ca38e8335102c13bc0792c3ccd2aa90b3
Piorosen <chacha@udon.party>
2024-06-08 11:59:17 +0900

    all test

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd)/tests:/tests -t piorosen/mnist /mnist file $@
+if ! docker image inspect piorosen/mnist-weight > /dev/null 2>&1; then
+    docker build -t piorosen/mnist-weight -f ./scripts/model_reproducibility.dockerfile ./scripts
+    id=$(docker run --rm -it --detach piorosen/mnist-weight)
+    echo $id
+    docker cp $id:/sources/output/weights.hpp .
+    docker cp $id:/sources/tests/ .
+    sudo docker kill $id
+fi
+
+if ! docker image inspect piorosen/mnist > /dev/null 2>&1; then
+    docker build -t piorosen/mnist -f ./scripts/mnist.dockerfile .
+fi
+
+# if [ -z "$1" ]; then
+#     docker run --rm -it -t piorosen/mnist /mnist input
+# fi
+
+file_list=($(ls "./tests" | grep ".txt"))
+for file in "${file_list[@]}"; do
+    echo $file
+    docker run --rm -it -v $(pwd)/tests:/tests -t piorosen/mnist /mnist file /tests/$file
+done
